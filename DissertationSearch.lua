d848ad3d522ba316d65b43dc11a9171296d97f82
Andrew Morgan <amorgan@atlas-sys.com>
2024-05-30 15:15:07 -0400

    Convert to WebView2.

--- DissertationSearch.lua
+++ DissertationSearch.lua
@@ -2,7 +2,7 @@
 --
 -- DissertationSearch.lua does a Dissertation Abstracts search for the LoanTitle for loans.
 -- autoSearch (boolean) determines whether the search is performed automatically when a request is opened or not.
---You must insert your client ID where the "XXXX". You can get this from your proquest url.
+-- You must insert your client ID where the "XXXX". You can get this from your proquest url.
 
 local autoSearch = GetSetting("AutoSearch");
 
@@ -11,16 +11,16 @@ local browser = nil;
 function Init()
 	if GetFieldValue("Transaction", "RequestType") == "Loan" then
 		interfaceMngr = GetInterfaceManager();
-		
+
 		-- Create browser
-                browser = interfaceMngr:CreateBrowser("Proquest Dissertation Abstracts", "Proquest Dissertation Abstracts", "Script");
-		
+        browser = interfaceMngr:CreateBrowser("Proquest Dissertation Abstracts", "Proquest Dissertation Abstracts", "Script", "WebView2");
+
 		-- Create buttons
-	  
-                browser:CreateButton("Search", GetClientImage("Search32"), "Search", "Proquest Dissertaion Abstracts");
-	  		
+
+        browser:CreateButton("Search", GetClientImage("Search32"), "Search", "Proquest Dissertaion Abstracts");
+
 		browser:Show();
-		
+
 		if autoSearch then
 			Search();
 		end
@@ -28,21 +28,22 @@ function Init()
 end
 
 function Search()
-     
-        browser:RegisterPageHandler("formExists", "searchForm","DissertationLoaded", false);
-     
-      
-         browser:Navigate("http://search.proquest.com/dissertations/advanced?accountid=" .. GetSetting("ClientId"));      
-      
+	browser:RegisterPageHandler("formExists", "searchForm","DissertationLoaded", false);
+
+	browser:Navigate("http://search.proquest.com/dissertations/advanced?accountid=" .. GetSetting("ClientId"));
 end
 
-      function DissertationLoaded()
-      browser:SetFormValue("adv_search_form", "queryTermField", GetFieldValue("Transaction", "LoanTitle"));
+function DissertationLoaded()
+	local title = GetFieldValue("Transaction", "LoanTitle");
+	
+	local searchFormScript = [[
+		(function (title) {
+			document.getElementsByName("queryTermField")[0].value = title;
 
-
-        browser:SubmitForm("adv_search_form");
-  
+			document.getElementsByName("adv_search_form")[0].submit();
+		})
+	]];
+	
+	
+	browser:ExecuteScript(searchFormScript, {title})
 end
-
- 
- 
